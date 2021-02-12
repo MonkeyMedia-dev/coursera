2b19836393b6e6901f1de0bea94dd95742bb0f56
Yuri Parrettini <yuri@monkeymedia.it>
2021-02-12 20:45:37 +0100

    My solution

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,7 +1,8 @@
 (function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for (first in names) {
-    if (names[first].charAt(0) == 'j' || names[first].charAt(0) == 'J') {
+    var letter = names[first].charAt(0).toLowerCase();
+    if (letter == 'j') {
       byeSpeaker.goodFunc(names[first])
     } else {
       helloSpeaker.helloFunc(names[first])
